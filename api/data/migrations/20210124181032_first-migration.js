5643af94ab4fbd1b37a0784ec9592efb5462dbcf
IsaacGHoward <howardisaac100@gmail.com>
2021-03-28 14:08:41 -0700

    Set up db structure and fixed a bug

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -7,8 +7,51 @@ exports.up = async (knex) => {
       users.string('user_email', 320).notNullable()
       users.timestamps(false, true)
     })
+    .createTable('category', (category) => {
+      category.increments('category_id')
+      category.string('name');
+    })
+    .createTable('role', (role) => {
+      role.increments('role_id')
+      role.string('name');
+    })
+    .createTable('items', (items) => {
+      items.increments('item_id')
+      items.string('item_name')
+        .notNullable()
+        .unique()
+      items.string('item_description')
+      items.string('item_price')
+        .notNullable()
+      items.string('item_location')
+        .notNullable()
+      items.integer('category_id')
+        .unsigned()
+        .notNullable()
+        .references('category_id')
+        .inTable('category')
+    })
+    .createTable('users_items', (usr_itm) => {
+      usr_itm.integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('user_id')
+        .inTable('users')
+      usr_itm.integer('item_id')
+        .unsigned()
+        .notNullable()
+        .references('item_id')
+        .inTable('items')
+    })
+
 }
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
+  await knex.schema
+    .dropTableIfExists('users')
+    .dropTableIfExists('category')
+    .dropTableIfExists('role')
+    .dropTableIfExists('items')
+    .dropTableIfExists('users_items')
+
 }
--- knexfile.js
+++ knexfile.js
@@ -37,8 +37,39 @@ module.exports = {
     connection: process.env.TESTING_DATABASE_URL,
   },
   production: {
+    client: "pg",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false}
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: { 
+      directory: './api/data/migrations' 
+    },
+    seeds: { 
+      directory: './api/data/seeds' 
+    }
+  },
+}
+
+/*
+module.exports = {
+  development: {
+    ...sharedConfig,
+    connection: process.env.DEV_DATABASE_URL,
+  },
+  testing: {
+    ...sharedConfig,
+    connection: process.env.TESTING_DATABASE_URL,
+  },
+  production: {
     ...sharedConfig,
     connection: process.env.DATABASE_URL,
     pool: { min: 2, max: 10 },
   },
 }
+
+*/
